a0778693f376ef6554be273b77561248acc843d5
SirTenzin <49116958+SirTenzin@users.noreply.github.com>
2024-03-25 21:28:11 +0000

    Add some color to the frame buttons

--- src/renderer/components/WindowActions.tsx
+++ src/renderer/components/WindowActions.tsx
@@ -4,7 +4,7 @@ function handleExit() {
 
 function CloseButton() {
   return (
-    <button className="btn btn-square col-start-5" onClick={handleExit}>
+    <button className="btn btn-square btn-primary col-start-5" onClick={handleExit}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
     </button>
   )
@@ -16,7 +16,7 @@ function handleMinimise() {
 
 function MinimiseButton() {
   return (
-    <button className="btn btn-square col-start-4" onClick={handleMinimise}>
+    <button className="btn btn-square btn-primary col-start-4" onClick={handleMinimise}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
       </svg>
@@ -30,7 +30,7 @@ function handleHome() {
 
 function HomeButton() {
   return (
-    <button className="btn btn-square col-start-3" onClick={handleHome}>
+    <button className="btn btn-square btn-primary col-start-3" onClick={handleHome}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
       </svg>
